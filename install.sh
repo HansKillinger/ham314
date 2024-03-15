7f0da1b9380568564c6d2e5f0a64ca4bdab1bf7e
Hans Killinger <hanskillinger@protonmail.com>
2024-03-14 20:59:36 -0500

    Update install.sh
    added Direwolf

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install libfltk1.3-dev libjpeg9-dev libxft-dev libxinerama-dev libxcursor-dev libsndfile1-dev libsamplerate0-dev portaudio19-dev libusb-1.0-0-dev install libpulse-dev install libudev-dev texinfo
+sudo apt-get install libfltk1.3-dev libjpeg9-dev libxft-dev libxinerama-dev libxcursor-dev libsndfile1-dev libsamplerate0-dev portaudio19-dev libusb-1.0-0-dev install libpulse-dev install libudev-dev texinfo -y
 sudo apt-get install -y libjpeg62-turbo-dev
 echo 'Building Hamlib 4.5.5'
 wget https://github.com/Hamlib/Hamlib/releases/download/4.5.5/hamlib-4.5.5.tar.gz
@@ -28,3 +28,13 @@ sudo make install
 cd ..
 echo 'Adding user bob to dialout'
 sudo usermod -a -G dialout bob
+echo 'Building Direwolf'
+sudo apt-get install git gcc g++ make cmake libasound2-dev libudev-dev -y
+git clone https://www.github.com/wb2osz/direwolf
+cd direwolf
+mkdir build && cd build
+cmake ..
+make -j4
+sudo make install
+make install-conf
+cd ..
