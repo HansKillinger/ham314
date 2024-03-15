2b7177b98b891c0f60a465f7c117cc14dabb1566
Hans Killinger <hanskillinger@protonmail.com>
2024-03-14 20:56:10 -0500

    Create install.sh
    compile hamlib, flrig, and fldigi

---
+++ install.sh
@@ -0,0 +1,30 @@
+sudo apt-get update
+sudo apt-get install libfltk1.3-dev libjpeg9-dev libxft-dev libxinerama-dev libxcursor-dev libsndfile1-dev libsamplerate0-dev portaudio19-dev libusb-1.0-0-dev install libpulse-dev install libudev-dev texinfo
+sudo apt-get install -y libjpeg62-turbo-dev
+echo 'Building Hamlib 4.5.5'
+wget https://github.com/Hamlib/Hamlib/releases/download/4.5.5/hamlib-4.5.5.tar.gz
+tar -zxvf hamlib-4.5.5.tar.gz
+cd hamlib-4.5.5
+./configure --prefix=/usr/local --enable-static
+make
+sudo make install
+sudo ldconfig
+cd ..
+echo 'Building Flrig 2.0.05'
+wget http://www.w1hkj.com/files/flrig/flrig-2.0.05.tar.gz
+tar -zxvf flrig-2.0.05.tar.gz
+cd flrig-2.0.05
+./configure --prefix=/usr/local --enable-static
+make
+sudo make install
+cd ..
+echo 'Building Fldigi 4.2.04'
+wget http://www.w1hkj.com/files/fldigi/fldigi-4.2.04.tar.gz
+tar -zxvf fldigi-4.2.04.tar.gz
+cd fldigi-4.2.04
+./configure --prefix=/usr/local --enable-static
+make
+sudo make install
+cd ..
+echo 'Adding user bob to dialout'
+sudo usermod -a -G dialout bob
