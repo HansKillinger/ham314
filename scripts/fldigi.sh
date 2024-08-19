2d83a57cf50788d601ad799be234e2f37f83608e
Hans Killinger <hanskillinger@protonmail.com>
2024-08-19 12:49:14 -0500

    Create fldigi.sh

---
+++ scripts/fldigi.sh
@@ -0,0 +1,8 @@
+echo 'Building Fldigi 4.2.04'
+wget http://www.w1hkj.com/files/fldigi/fldigi-4.2.05.tar.gz
+tar -zxvf fldigi-4.2.05.tar.gz
+cd fldigi-4.2.05
+./configure --prefix=/usr/local --enable-static
+make
+sudo make install
+cd ~/build
