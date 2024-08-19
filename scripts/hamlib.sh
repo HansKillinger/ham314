29d9720224fe79a62be3f5b49043c0c8e8b32426
Hans Killinger <hanskillinger@protonmail.com>
2024-08-19 12:47:48 -0500

    Create hamlib.sh

---
+++ scripts/hamlib.sh
@@ -0,0 +1,12 @@
+echo 'Building Hamlib 4.5.5'
+sudo apt-get update
+sudo apt-get install libfltk1.3-dev libjpeg9-dev libxft-dev libxinerama-dev libxcursor-dev libsndfile1-dev libsamplerate0-dev portaudio19-dev libusb-1.0-0-dev libpulse-dev libudev-dev texinfo -y
+sudo apt-get install -y libjpeg62-turbo-dev
+wget https://github.com/Hamlib/Hamlib/releases/download/4.5.5/hamlib-4.5.5.tar.gz
+tar -zxvf hamlib-4.5.5.tar.gz
+cd hamlib-4.5.5
+./configure --prefix=/usr/local --enable-static
+make
+sudo make install
+sudo ldconfig
+cd ~/build
