4ebf2c7956bbe2499277017567d16f9b3abc9f2a
Hans Killinger <hanskillinger@protonmail.com>
2024-08-19 12:56:40 -0500

    Update chirp.sh

--- scripts/chirp.sh
+++ scripts/chirp.sh
@@ -1,4 +1,5 @@
 echo 'Install Chirp'
+cd ~/build
 sudo apt install git python3-wxgtk4.0 python3-serial python3-six python3-future python3-requests python3-pip -y
 wget https://archive.chirpmyradio.com/chirp_next/next-20240312/chirp-20240312-py3-none-any.whl
 pip install ./chirp-20240312-py3-none-any.whl
