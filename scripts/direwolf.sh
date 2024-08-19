489010f3d5891868d6f6b3925e6ca7ef80a1f6f1
Hans Killinger <hanskillinger@protonmail.com>
2024-08-19 12:50:03 -0500

    Create direwolf.sh

---
+++ scripts/direwolf.sh
@@ -0,0 +1,10 @@
+echo 'Building Direwolf'
+sudo apt-get install git gcc g++ make cmake libasound2-dev libudev-dev -y
+git clone https://www.github.com/wb2osz/direwolf
+cd direwolf
+mkdir build && cd build
+cmake ..
+make -j4
+sudo make install
+make install-conf
+cd ~/build
