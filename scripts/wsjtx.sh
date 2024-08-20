8478dcc9b623fd666ec91c32dcffee0649d03a4e
Hans Killinger <hanskillinger@protonmail.com>
2024-08-19 22:11:56 -0500

    Update wsjtx.sh

--- scripts/wsjtx.sh
+++ scripts/wsjtx.sh
@@ -1,4 +1,5 @@
 echo 'Install WSJT-X'
 cd ~/build
-wget https://sourceforge.net/projects/wsjt/files/wsjtx-2.6.1/wsjtx_2.6.1_armhf.deb -y
+wget https://sourceforge.net/projects/wsjt/files/wsjtx-2.6.1/wsjtx_2.6.1_armhf.deb
+sudo dpkg -i wsjtx_2.6.1_armhf.deb
 sudo apt --fix-broken install -y
