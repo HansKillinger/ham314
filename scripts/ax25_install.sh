314649e56ece1a9cddb2bb04dd5e357eb1b8b4ea
Hans Killinger <hanskillinger@protonmail.com>
2024-08-21 13:31:07 -0500

    Update ax25_install.sh

--- scripts/ax25_install.sh
+++ scripts/ax25_install.sh
@@ -3,6 +3,8 @@ cd deb
 sudo dpkg -i direwolf*.deb -y
 sudo apt-get install ax25-tools ax25-node ax25-apps telnet uronode
 sudo cp ../conf/ax25/* /etc/ax25
+sudo touch /etc/ax25/nodebackup.sh
+sudo chmod u+x /etc/ax25/nodebackup.sh
 sudo echo "uronode       4444/tcp        # KD9YQK packet node" >> /etc/services
 sudo echo "uronode stream tcp nowait root /usr/sbin/uronode uronode" >> /etc/inetd.conf
 sudo service inetd restart
