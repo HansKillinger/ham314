7e659f08aa20f9c0abc3433e79af15867e41f5e5
Hans Killinger <hanskillinger@protonmail.com>
2024-08-22 11:23:13 -0500

    Update ramdrives.sh

--- scripts/ramdrives.sh
+++ scripts/ramdrives.sh
@@ -1,7 +1,10 @@
 echo "Setting up ramdrives to preserve SD-Card"
+sleep 1
 sudo echo "tmpfs /tmp tmpfs defaults,noatime,nosuid,size=100m 0 0" | sudo tee -a /etc/fstab
 sudo echo "tmpfs /var/tmp tmpfs defaults,noatime,nosuid,size=30m 0 0" | sudo tee -a /etc/fstab
 sudo echo "tmpfs /var/log tmpfs defaults,noatime,nosuid,mode=0755,size=100m 0 0" | sudo tee -a /etc/fstab
 sudo echo "tmpfs /var/run tmpfs defaults,noatime,nosuid,mode=0755,size=2m 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
+echo "Ramdrives created"
+sleep 1
 df -h
