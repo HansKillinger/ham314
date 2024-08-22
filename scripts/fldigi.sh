34a3ac6d8a6c6fe5a0aea18f379db140e94729df
Hans Killinger <hanskillinger@protonmail.com>
2024-08-22 16:26:30 -0500

    Update fldigi.sh

--- scripts/fldigi.sh
+++ scripts/fldigi.sh
@@ -4,5 +4,5 @@ wget http://www.w1hkj.com/files/fldigi/fldigi-4.2.05.tar.gz
 tar -zxvf fldigi-4.2.05.tar.gz
 cd fldigi-4.2.05
 ./configure --prefix=/usr/local --enable-static
-#make -j 4
-#sudo make install
+make -j 4
+sudo checkinstall -D make install
