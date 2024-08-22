4a5236d37f766f9d29a3f80067ee150e4887e859
Hans Killinger <hanskillinger@protonmail.com>
2024-08-22 12:22:59 -0500

    Update js8call.sh

--- scripts/js8call.sh
+++ scripts/js8call.sh
@@ -1,4 +1,6 @@
 echo 'Install js8call 2.2.0'
-cd ~/build
-wget http://files.js8call.com/2.2.0/js8call_2.2.0_armhf.deb
-sudo dpkg -i js8call_2.2.0_armhf.deb
+sleep 1
+sudo apt install js8call -y
+#cd ~/build
+#wget http://files.js8call.com/2.2.0/js8call_2.2.0_armhf.deb
+#sudo dpkg -i js8call_2.2.0_armhf.deb
