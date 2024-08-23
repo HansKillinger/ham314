f987764e36b80bee0722f899573b205a31a5ff13
Hans Killinger <hanskillinger@protonmail.com>
2024-08-22 19:37:51 -0500

    Update ax25service.sh

--- launchers/ax25service.sh
+++ launchers/ax25service.sh
@@ -16,8 +16,8 @@ sudo killall ax25d
 sudo killall kissattach
 sudo killall direwolf
 
-# zero out old direwolf log file in case /run/ is full
-truncate --size 0 /var/log/direwolf.log
+# zero out old direwolf log file in case /tmp/ is full
+truncate --size 0 /tmp/direwolf.log
 
 sudo -u kd9yqk direwolf -t 0 -c /home/kd9yqk/direwolf-node.conf -p > /tmp/direwolf.log &
 
@@ -28,19 +28,15 @@ if [ $nrdevice -eq 0 ]; then
    sudo nrattach netrom  # run this once per boot
 fi
 
-#if [ -n "$WINLINKALSO" ]; then
-#  sudo kissattach `ls -l /tmp/kisstnc | awk '{ print $11 }'` radio 44.56.4.222 # set IP, which makes winlink advertise
-#else
-#  sudo kissattach `ls -l /tmp/kisstnc | awk '{ print $11 }'` radio             # do not set IP, no winlink advertisement
-#fi
+sudo kissattach `ls -l /tmp/kisstnc | awk '{ print $11 }'` ax0
 
-sudo kissparms -c 1 -p radio  # fix invalid port first to tries on direwolf
-sudo ax25d  # for rmsgw only
-sudo /etc/ax25/nodebackup.sh  # restore node table and routes we heard last time
-sudo /usr/sbin/netromd        # Start the netrom service, lists for nodes/routes
-#sudo route del -net 44.0.0.0 netmask 255.0.0.0  # kill tcp traffic to ax0, updatesysop.py hits api.winlink.org on net 44
+sudo kissparms -c 1 -p ax0  # fix invalid port first to tries on direwolf
+sudo ax25d
+sudo /etc/ax25/nodebackup.sh
+sudo /usr/sbin/netromd
+sudo mheardd
 
-# advertise node on aprs.fi
+# backup node data hourly
 while true; do
    sleep 1200
    sudo nodesave /etc/ax25/nodebackup.sh
