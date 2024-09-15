908a2ecf50032155893f3b7aab7e80cf78df953d
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-09-15 14:42:06 -0500

    Update pcsi.sh

--- scripts/pcsi.sh
+++ scripts/pcsi.sh
@@ -2,6 +2,6 @@ echo 'Build PCSI'
 cd ~
 git clone https://github.com/maqifrnswa/PCSI.git
 python -m venv PCSI/
-sudo apt install python3-opencv python3-tk python3-pil.imagetk -y
-PCSI/bin/pip install numpy imageio pylbfgs pyserial pillow
-PCSI/bin/pip install bitstring==4.1.4
+#sudo apt install python3-opencv python3-tk python3-pil.imagetk -y
+PCSI/bin/pip install opencv-python numpy==2.0.2 imageio bitstring==4.1.4 pylbfgs pyserial pillow
+
