d331d6887478d190ce33cc608df0a289a5016662
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-09-15 12:16:50 -0500

    Update ax25_install.sh

--- scripts/ax25_install.sh
+++ scripts/ax25_install.sh
@@ -6,8 +6,10 @@ sudo dpkg -i ../deb/ax25apps*.deb
 sudo dpkg -i ../deb/ax25tools*.deb
 sudo dpkg -i ../deb/libax25-dev*.deb
 sudo dpkg -i ../deb/libax25_1*.deb
+sudo dpkg -i ../deb/uronode_*.deb
 #sudo apt-get install ax25-tools ax25-apps telnet ax25-node -y
-sudo apt install uronode openbsd-inetd tcpd -y
+#sudo apt install uronode openbsd-inetd tcpd -y
+sudo apt install telnet openbsd-inetd tcpd -y
 sudo cp ../conf/ax25/* /etc/ax25
 sudo touch /etc/ax25/nodebackup.sh
 sudo chmod u+x /etc/ax25/nodebackup.sh
