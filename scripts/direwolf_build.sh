6d02e295d10c28515b00a6ecd7566d224f2aa44c
Hans Killinger <hanskillinger@protonmail.com>
2024-10-08 13:05:07 -0500

    Update direwolf_build.sh

--- scripts/direwolf_build.sh
+++ scripts/direwolf_build.sh
@@ -3,12 +3,14 @@ cd ~/build
 sudo apt-get install git gcc g++ make cmake libasound2-dev libudev-dev -y
 git clone https://www.github.com/wb2osz/direwolf
 cd direwolf
+git branch -r
+git checkout dev
 mkdir build && cd build
 cmake ..
 make -j4
-sudo make install
-make install-conf
+#sudo make install
+#make install-conf
 
-#sudo cpack -G DEB
-#sudo apt install ./direwolf-*.deb
-#sudo apt-mark hold direwolf
+sudo cpack -G DEB
+sudo apt install ./direwolf-*.deb
+sudo apt-mark hold direwolf
